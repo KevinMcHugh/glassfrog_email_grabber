1f334f5c7fd64b3ed7a4e569a6828e23bb4acad7
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-10-31 13:49:06 -0500

    Fixed command line clients no circle case.
    I should probably be writing tests.

--- compose.rb
+++ compose.rb
@@ -4,7 +4,7 @@ require_relative 'email_grabber'
 
 circle = ARGV.first
 renderer = ERB.new(File.read("compose.erb"))
-addresses = GrabberPrinter.new.get_emails circle
+addresses = GrabberPrinter.new.get_emails circle, ENV['GLASSFROG_KEY']
 Open3.pipeline_w(["osascript"]) {|i, ts|
   i.puts(renderer.result())
 }
--- get_emails.rb
+++ get_emails.rb
@@ -1,4 +1,4 @@
 require_relative 'email_grabber'
 
 circle = ARGV.first
-puts GrabberPrinter.new.get_emails circle
+puts GrabberPrinter.new.get_emails circle, ENV['GLASSFROG_KEY']
