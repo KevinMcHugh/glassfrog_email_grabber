beb41e27012280ef4473d41853fae470e1c58be2
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-11-11 21:46:59 -0600

    Added positive test case of CGW

--- spec/console_grabber_wrapper_spec.rb
+++ spec/console_grabber_wrapper_spec.rb
@@ -9,15 +9,25 @@ describe ConsoleGrabberWrapper do
 		"Something went wrong. Here's a list of circles I know about. \n JCVD \n AS"
 	end
 	describe '#get_emails' do
-		before(:each) do
-			EmailGrabber.any_instance.stub(:get_emails_for).and_raise(ArgumentError.new)
-			EmailGrabber.any_instance.stub(:get_circles).and_return(circles)
+		context "with no circle input" do
+			before(:each) do
+				EmailGrabber.any_instance.stub(:get_emails_for).and_raise(ArgumentError.new)
+				EmailGrabber.any_instance.stub(:get_circles).and_return(circles)
+			end
+			def get_emails  
+				ConsoleGrabberWrapper.new.get_emails nil, ""
+			end
+			it "raises an exception" do
+				expect {get_emails}.to raise_error ArgumentError, error_message
+			end
 		end
-		def get_emails  
-			ConsoleGrabberWrapper.new.get_emails nil, ""
-		end
-		it "raises an exception" do
-			expect {get_emails}.to raise_error ArgumentError, error_message
+		context "with a circle input" do
+			let(:people_result) { ["jkimble@kindergartencop.com"] }
+			before(:each) do
+				EmailGrabber.any_instance.stub(:get_emails_for).and_return(people_result)
+			end
+			subject { ConsoleGrabberWrapper.new.get_emails nil, "" }
+			it {should == people_result}
 		end
 	end
 end
