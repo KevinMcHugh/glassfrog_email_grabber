889dfe2149845f745cf6455dadb56ea67703c971
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-11-11 20:47:47 -0600

    Airplane coding causes bugs. Don't do it.

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'haml'
 require_relative 'email_grabber'
+require_relative 'web_grabber_wrapper'
+
 
 get '/' do
 	@members = []
@@ -9,11 +11,13 @@ end
 
 post '/' do
 	circle = params[:circle]
-	if params[:api_key].empty?
+	api_key = params[:api_key]
+	if !api_key || api_key.empty?
 		@error = "please enter an api key"
 		@members = nil
 	else
 		grabber = WebGrabberWrapper.new
+		grabber.get_emails({ circle: circle, api_key: api_key})
 		@error = grabber.error
 		@members = grabber.members
 		@mailto = grabber.mailto
