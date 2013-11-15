8cf66d61afd95b162161b243d866831248013347
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-11-15 17:17:17 -0600

    api key is saved as a cookie now. This code's ugly.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby "2.0.0"
 gem 'nokogiri'
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'haml'
 group :development, :test do
 	gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,20 +1,32 @@
 require 'sinatra'
 require 'haml'
+require "sinatra/cookies"
 require_relative 'email_grabber'
 require_relative 'web_grabber_wrapper'
 
 
 get '/' do
 	@members = []
+	api_key = cookies[:api_key]
+	redirect to('/circles') if api_key
+	haml :set_cookie
+end
+
+get '/circles' do
+	api_key = cookies[:api_key]
+	grabber = WebGrabberWrapper.new
+	result = grabber.get_emails({ circle: '', api_key: api_key})
+	@error = result[:error]
+	@members = result[:members]
+	@mailto = result[:mailto]
 	haml :list
 end
 
-post '/' do
+post '/circles' do
 	circle = params[:circle]
-	api_key = params[:api_key]
+	api_key = cookies[:api_key]
 	if !api_key || api_key.empty?
-		@error = "please enter an api key"
-		@members = nil
+		redirect to('/')
 	else
 		grabber = WebGrabberWrapper.new
 		result = grabber.get_emails({ circle: circle, api_key: api_key})
@@ -22,5 +34,10 @@ post '/' do
 		@members = result[:members]
 		@mailto = result[:mailto]
 	end
+	haml :list
+end
+
+post '/' do
+	response.set_cookie :api_key, params[:api_key]
 	haml :list
 end
